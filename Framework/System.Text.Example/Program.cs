e676ec5caf3a5381f72760837447ffe1c6406f9b
Carlos Castro <carloscastrogames@gmail.com>
2018-04-19 02:11:57 -0300

    StringBuilder
    StringBuilder

---
+++ Framework/System.Text.Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace System.Text.Example
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder _stringBuilder = new StringBuilder();
+
+            _stringBuilder.Append("Carlos ");
+            _stringBuilder.Append("Castro");
+
+
+            //Insert After six words
+            _stringBuilder.Insert(6, " Alberto ");
+
+            //Remove After six Words, eight Words
+            _stringBuilder.Remove(6, 8);
+
+            //Replace 
+            _stringBuilder.Replace(" ", "-");
+
+            Console.WriteLine(_stringBuilder);
+            Console.ReadKey();
+        }
+    }
+}
