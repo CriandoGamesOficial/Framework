5aaf924c299832b4b89823bf1450702c3171ead1
Carlos Castro <carloscastrogames@gmail.com>
2018-04-21 00:49:09 -0300

    System.IO.File

---
+++ Framework/System.IO.Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace System.IO.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("My name is carlos , i have one dog j");
+            File.AppendAllText(@"C:\Users\POSITIVO\Desktop\1.txt", stringBuilder.ToString());
+
+            //string[] line = { "line 1", "line 2 ", "line 3" };
+            // File.AppendAllLines(@"C:\Users\POSITIVO\Desktop\2.txt", line);
+
+            //Encrypt File 
+            // File.Encrypt(@"C:\Users\POSITIVO\Desktop\2.txt");
+
+
+            File.Decrypt(@"C:\Users\POSITIVO\Desktop\2.txt");
+
+        }
+    }
+}
