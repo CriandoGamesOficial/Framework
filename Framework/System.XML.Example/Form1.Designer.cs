cd399828087e4a5da0654a30ed5924997190d44e
Carlos Castro <carloscastrogames@gmail.com>
2018-04-21 02:14:08 -0300

    System.XML
    part 1 criando e carregando dados em um Xml

---
+++ Framework/System.XML.Example/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace System.XML.Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsalvar = new System.Windows.Forms.Button();
+            this.textnome = new System.Windows.Forms.TextBox();
+            this.textTelefone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnsalvar
+            // 
+            this.btnsalvar.Location = new System.Drawing.Point(87, 172);
+            this.btnsalvar.Name = "btnsalvar";
+            this.btnsalvar.Size = new System.Drawing.Size(100, 23);
+            this.btnsalvar.TabIndex = 0;
+            this.btnsalvar.Text = "Salvar";
+            this.btnsalvar.UseVisualStyleBackColor = true;
+            this.btnsalvar.Click += new System.EventHandler(this.btnsalvar_Click);
+            // 
+            // textnome
+            // 
+            this.textnome.Location = new System.Drawing.Point(87, 39);
+            this.textnome.Name = "textnome";
+            this.textnome.Size = new System.Drawing.Size(100, 20);
+            this.textnome.TabIndex = 1;
+            // 
+            // textTelefone
+            // 
+            this.textTelefone.Location = new System.Drawing.Point(87, 103);
+            this.textTelefone.Name = "textTelefone";
+            this.textTelefone.Size = new System.Drawing.Size(100, 20);
+            this.textTelefone.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 274);
+            this.Controls.Add(this.textTelefone);
+            this.Controls.Add(this.textnome);
+            this.Controls.Add(this.btnsalvar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Windows.Forms.Button btnsalvar;
+        private Windows.Forms.TextBox textnome;
+        private Windows.Forms.TextBox textTelefone;
+    }
+}
+
---
+++ Framework/System.XML.Example/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace System.XML.Example
+{
+    public partial class Form1 : Form
+    {
+        //caminha pre definido
+        string arquivo = @"C:\Users\POSITIVO\Desktop\contato.xml";
+
+        //criando variavel XmlDocument 
+        XmlDocument xmlDocument = new XmlDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+            if(!File.Exists(arquivo))
+            {
+                //criando arquivo Xml 
+                XmlNode nodeRoot = xmlDocument.CreateElement("Contato");
+                //colocando o documento dentro do diretorio Raiz
+                xmlDocument.AppendChild(nodeRoot);
+                //usando o save e passando o caminho
+                xmlDocument.Save(arquivo);
+            }
+        }
+
+        private void btnsalvar_Click(object sender, EventArgs e)
+        {
+            xmlDocument.Load(arquivo);
+            //criando os nodes a serem atribuidos
+            XmlNode xmlNome = xmlDocument.CreateElement("Nome");
+            XmlNode xmlTelefone = xmlDocument.CreateElement("Telefone");
+
+            //Get informacoes do TextInbox e armazendo em um node
+            xmlNome.InnerText = textnome.Text;
+            xmlTelefone.InnerText = textTelefone.Text;
+
+            //passando o node de gravacao e rederenciado os dados a serem gravados
+            xmlDocument.SelectSingleNode("/Contato").AppendChild(xmlNome);
+            xmlDocument.SelectSingleNode("/Contato").AppendChild(xmlTelefone);
+
+
+            //salvando arquivo
+            xmlDocument.Save(arquivo);
+
+        }
+    }
+}
